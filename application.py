0b886393afa31fc5467da4ea132017a726497ecf
Aswin Antony <aswinajraj@gmail.com>
2020-06-21 21:32:45 +0530

    Deleted hello.py and updated application.py

--- application.py
+++ application.py
@@ -7,3 +7,8 @@ def index():
 	headline = "Hello, world! from Python Code" #Variable headline is created
 	return render_template("index.html", headline=headline)
 	#now here the variable is assigned to the headline attribute in index.html
+
+####For running Flask
+# $ export FLASK_APP=hello.py
+# $ flask run
+# * Running on http://127.0.0.1:5000/
