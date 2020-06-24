bb23e2770b8d332a8026b2d9c64e8653cae634a5
Aswin Antony <aswinajraj@gmail.com>
2020-06-24 18:56:49 +0530

    The function name is the value within url_for

--- application.py
+++ application.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
+	return render_template("index.html")
 	#headline = "Hello, world! from Python Code" #Variable headline is created
 	#return render_template("index.html", headline=headline)
 	#now here the variable is assigned to the headline attribute in index.html
 
-
-    return render_template("index.html")
-
 @app.route("/more")
-def more():
+def see_more():
     return render_template("more.html")
 
 ####For running Flask
