6c82645117da7905b222b08c9cfe5a42e7099365
aswinantony <aswinajraj@gmail.com>
2020-06-24 17:27:27 +0530

    Extending functionality of Layout in HTML

--- application.py
+++ application.py
@@ -4,11 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	headline = "Hello, world! from Python Code" #Variable headline is created
-	return render_template("index.html", headline=headline)
+	#headline = "Hello, world! from Python Code" #Variable headline is created
+	#return render_template("index.html", headline=headline)
 	#now here the variable is assigned to the headline attribute in index.html
 
+
+    return render_template("index.html")
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
+
 ####For running Flask
-# $ export FLASK_APP=hello.py
+# $ export FLASK_APP=application.py
 # $ flask run
 # * Running on http://127.0.0.1:5000/
